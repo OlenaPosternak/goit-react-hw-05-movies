999712a7bbe0aeb9ecb8f0f0bcb073663267c86e
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-11-07 17:00:25 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,13 +26,12 @@ export const App = () => {
         <Line />
       </Container>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Home />} />
         <Route path="movies" element={<Movies />} />
         <Route path="movies/:id" element={<MovieInfo />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<Home />} />
       </Routes>
     </>
   );
